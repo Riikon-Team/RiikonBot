5ebb2522afb3ccc16437558c5d9988cd4cbffcda
Riichan04 <22130274@st.hcmuaf.edu.vn>
2025-04-19 22:31:05 +0700

    Add press F command

---
+++ src/packages/social/commands/pressF.js
@@ -0,0 +1,22 @@
+import { displayCurrentTime } from '../../../utils/commandUtilities.js';
+import { EmbedBuilder } from 'discord.js'
+
+//Just temporary code too
+const image = "https://media.tenor.com/enlgLAA9qnsAAAAM/press-f-pay-respect.gif"
+
+export const config = {
+    name: 'f',
+    description: 'Press F to pay respect',
+    category: 'Meme',
+    options: []
+}
+
+export async function execute(interaction, client) {
+    const user = interaction.user
+    const embed = new EmbedBuilder()
+        .setTitle(`${user.username} has pressed F to pay respect`)
+        .setImage(image)
+        .setFooter({text: `Send at ${displayCurrentTime()}`})
+
+    await interaction.reply({embeds: [embed]})
+}
