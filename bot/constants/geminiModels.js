1d56fa723360c6fed4747e797acd08c9b60c07ce
Trieu Nguyen Thanh <trieukon1011@gmail.com>
2025-11-20 15:26:21 +0700

    feat: update model

--- bot/constants/geminiModels.js
+++ bot/constants/geminiModels.js
@@ -6,6 +6,7 @@ export const GEMINI_2_5_FLASH = 'gemini-2.5-flash';
 // export const GEMINI_2_5_IMAGE = 'gemini-2.5-flash-image-preview';
 export const GEMINI_2_5_IMAGE = 'gemini-2.0-flash-preview-image-generation';
 export const GEMINI_2_5_LITE = 'gemini-2.5-flash-lite-preview-09-2025';
+export const GEMINI_3 = 'gemini-3-pro-preview';
 
 export const GEMINI_MODELS = {
     GEMINI_2_5_FLASH: {
@@ -24,20 +25,22 @@ export const GEMINI_MODELS = {
             "Limited context window compared to larger models"
         ]
     },
-    // GEMINI_2_5_IMAGE: {
-    //     id: GEMINI_2_5_IMAGE,
-    //     description: "A model optimized for image-related tasks, including image generation, captioning, and understanding visual content.",
-    //     capabilities: [
-    //         "Image Generation",
-    //         "Image Captioning",
-    //         "Visual Content Analysis",
-    //         "Multimodal Understanding"
-    //     ],
-    //     limitations: [
-    //         "Not suitable for text-only tasks",
-    //         "May have limitations in understanding complex visual scenes"
-    //     ]
-    // },
+    GEMINI_3: {
+        id: GEMINI_3,
+        description: "A model optimized for image-related tasks, including image generation, captioning, and understanding visual content. It works well for applications that require multimodal capabilities, and thinking so amazing. Recommended!!!",
+        capabilities: [
+            "Visual Content Analysis",
+            "Multimodal Understanding",
+            "Captioning",
+            "Thinking",
+            "Code Generation (so amazing!)",
+            "Research"
+        ],
+        limitations: [
+            "Not suitable for text-only tasks",
+            "May have limitations in understanding complex visual scenes"
+        ]
+    },
     GEMINI_2_5_LITE: {
         id: GEMINI_2_5_LITE,
         description: "A balanced model that offers a good trade-off between performance and speed, suitable for a variety of applications.",
